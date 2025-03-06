c82e7c3339e1fa6099cca8e1722abcf9492986a6
Vincent <vincent00lav@outlook.com>
2025-03-06 15:19:28 +0100

    QuizAccordion initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.scss";
+import QuizAccordion from "./components/QuizAccordion";
 
 function App() {
   return (
     <>
       <h1>Quiz Intéractif</h1>
+      <QuizAccordion />
     </>
   );
 }
